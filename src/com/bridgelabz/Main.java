7d483951d53d20821e4e9a3acea8f0eb47a1e526
Chougulenik <nikitachougule47@gmail.com>
2022-03-23 14:27:23 -0400

    UC-2 added

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -7,6 +7,9 @@ public class Main {
 		Maximum max = new Maximum();
 		Integer[] intArray = {1,4,3};
 		max.maxElement(intArray);
+		
+		Float[] intFloat = {1.1f,2.2f,3.4f};
+		max.maxElement(intFloat);
 
 	}
 
--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -10,4 +10,13 @@ public class Maximum {
 		}
 		System.out.println(intArray[intArray.length-1]);
 	}
+	
+	public void maxElement(Float[] intArray) {
+		for(int i=0; i<intArray.length-1; i++) {
+			if(intArray[i].compareTo(intArray[i+1]) == 1) {
+				intArray[i+1] = intArray[i];
+			}
+		}
+		System.out.println(intArray[intArray.length-1]);
+	}
 }
